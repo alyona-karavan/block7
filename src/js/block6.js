3df6e296982cf4e7a3f31f996293203c5248900c
Alyona_Karavan <alyonakaravan99@gmail.com>
2024-05-08 21:25:03 +0300

    пофиксила пагинацию и js на 6 блок

--- src/js/block6.js
+++ src/js/block6.js
@@ -6,49 +6,13 @@ let arrow = document.querySelector('.arrowFix-open')
 fixButton.addEventListener('click', function (evt) {
   evt.preventDefault()
 
-  arrow.src = 'img/logo/ic/expand-1.png'
-  fixWrapper.style.height = '100%'
-  fixButtonText.textContent = 'Скрыть'
-
-  if (fixButtonText.textContent === 'Скрыть') {
-    fixButton.addEventListener('click', function (evt) {
-      evt.preventDefault()
-
-      arrow.src = 'img/logo/ic/expand.png'
-      fixButtonText.textContent = 'Показать всё'
-      fixWrapper.style.height = '160px'
-    })
+  if (fixButtonText.textContent === 'Показать всё') {
+    arrow.src = 'img/logo/ic/expand-1.png'
+    fixWrapper.style.height = '100%'
+    fixButtonText.textContent = 'Скрыть'
+  } else {
+    arrow.src = 'img/logo/ic/expand.png'
+    fixButtonText.textContent = 'Показать всё'
+    fixWrapper.style.height = '160px'
   }
 })
-
-// fixButton.addEventListener('click', function (evt) {
-//   evt.preventDefault()
-
-//   if (fixButtonText.textContent === 'Показать всё') {
-//     arrow.src = 'img/logo/ic/expand-1.png'
-//     fixWrapper.style.height = '100%'
-//     fixButtonText.textContent = 'Скрыть'
-//   }
-
-//   if (fixButtonText.textContent === 'Скрыть') {
-//     arrow.src = 'img/logo/ic/expand.png'
-//     fixButtonText.textContent = 'Показать всё'
-//     fixWrapper.style.height = '160px'
-//   }
-// })
-
-// function handleClick() {
-//   if (fixButtonText.textContent === 'Показать всё') {
-//     arrow.src = 'img/logo/ic/expand-1.png'
-//     fixWrapper.style.height = '100%'
-//     fixButtonText.textContent = 'Скрыть'
-//   }
-
-//   if (fixButtonText.textContent === 'Скрыть') {
-//     arrow.src = 'img/logo/ic/expand.png'
-//     fixButtonText.textContent = 'Показать всё'
-//     fixWrapper.style.height = '160px'
-//   }
-// }
-
-// fixButton.addEventListener('click', handleClick)
